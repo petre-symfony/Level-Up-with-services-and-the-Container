d64fa0919c77a7c99363f9ff5d3d089c2d9e506d
petrero <petrero@gmail.com>
2017-06-05 08:58:23 +0300

    Copy code from branch 3

---
+++ src/AppBundle/Service/MarkdownTransformer.php
@@ -0,0 +1,16 @@
+<?php
+namespace AppBundle\Service;
+
+
+class MarkdownTransformer {
+  private $markdownParser;
+  
+  public function __construct($markdownParser) {
+    $this->markdownParser = $markdownParser;
+  }
+
+  public function parse($str){
+    return $this->markdownParser
+      ->transform($str);
+  }
+}
