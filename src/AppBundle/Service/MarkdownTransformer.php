86ec13e0713cdd39052a6d2d701fe2e6e083559a
petrero <petrero@gmail.com>
2017-06-05 09:55:08 +0300

    Phew! Dependency injection, check! Registering new services, check! Delicious snack, check! Well, I hope you just had a delicious snack.
    This tutorial is the start to our victory lap. We need to add caching to MarkdownTransformer: it should be pretty easy. Copy part of the old caching code and paste that into the parse() function. Remove the else part of the if and just return {dollar_sign}cache->fetch().
    
    Below, assign the method call to the  variable and go copy the old {dollar_sign}cache->save() line. Return {dollar_sign}str and re-add the sleep() call so that things are really slow - that keeps it interesting.
    
    On top, change the  variables to . Perfect!
    
    We know this won't work: there is no get() function in this class. And more importantly, we don't have access to the doctrine_cache.provider.my_markdown_cache service. How can we get access? Dependency injection.

--- src/AppBundle/Service/MarkdownTransformer.php
+++ src/AppBundle/Service/MarkdownTransformer.php
@@ -11,7 +11,17 @@ class MarkdownTransformer {
   }
 
   public function parse($str){
-    return $this->markdownParser
+    $cache = $this->get('doctrine_cache.providers.my_markdown_cache');
+    $key = md5($str);
+    if ($cache->contains($key)) {
+      return $cache->fetch($key);
+    }
+    
+    sleep(1);
+    $str = $this->markdownParser
       ->transformMarkdown($str);
+    $cache->save($key, $str);
+    return $str;
+    
   }
 }
