3e058494bfb995ad6ab0eccec9fb5c61807bf053
petrero <petrero@gmail.com>
2017-06-05 10:41:23 +0300

    Copy code from branch 7

---
+++ src/AppBundle/Twig/MarkdownExtension.php
@@ -0,0 +1,19 @@
+<?php
+namespace AppBundle\Twig;
+
+
+class MarkdownExtension extends \Twig_Extension {
+  public function getFilters() {
+    return [
+      new \Twig_SimpleFilter('markdownify', array($this, 'parseMarkdown'))    
+    ];
+  }
+  
+  public function parseMarkdown($str){
+    return strtoupper($str);
+  }
+
+  public function getName(){
+    return 'app_markdown';
+  }
+}
