c7324f8bdc61a7994dfc0efd4940346b96611e18
petrero <petrero@gmail.com>
2017-06-05 10:15:07 +0300

    Remove sleep line

--- src/AppBundle/Service/MarkdownTransformer.php
+++ src/AppBundle/Service/MarkdownTransformer.php
@@ -20,7 +20,6 @@ class MarkdownTransformer {
       return $cache->fetch($key);
     }
     
-    sleep(1);
     $str = $this->markdownParser
       ->transformMarkdown($str);
     $cache->save($key, $str);
